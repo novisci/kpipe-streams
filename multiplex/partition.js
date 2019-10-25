f2e8adc4d954258e644b379a41a3c711390771bf
Paul Rowan <prowan@novisci.com>
2019-10-25 09:45:13 -0400

    Reworked multiplex/partition to correctly stop on newline boundaaries

--- multiplex/partition.js
+++ multiplex/partition.js
@@ -17,8 +17,8 @@ module.exports = function (options) {
   //  and each chunk of incoming data (a reduce operation).
   //  The state object may be modified and is returned back
   //  on successive calls to bound(),
-  //  A return value of true indicates that a partition boundary
-  //  has been reaches and the next stream will be requested
+  //  The integer return indicates the offset into the string/buffer
+  //  for the end of partition. If the end is not reached, -1 is returned
   if (typeof options.bound !== 'function') {
     throw Error('options.bound must be a function')
   }
@@ -42,6 +42,7 @@ module.exports = function (options) {
 
   // let count = 0
   let paused = false
+  let remainder = null
 
   function _write (chunk, enc, cb) {
     if (paused) {
@@ -50,10 +51,29 @@ module.exports = function (options) {
       return false
     }
 
+    if (!Buffer.isBuffer(chunk)) {
+      return cb(Error('partition currently implements buffer streams only'))
+    }
+
+    // Split the buffer by the returned partition end offset (unless -1)
+    const off = fnBound(state, chunk)
+    if (off >= 0) {
+      remainder = chunk.slice(off + 1)
+      // console.error('OFF ' + off)
+      // console.error('LENGTH ' + chunk.length)
+      // console.error('REMAIN ' + remainder)
+      chunk = chunk.slice(0, off + 1) // Buffer.from(chunk, 0, off)
+    } else if (remainder) {
+      chunk = Buffer.concat([remainder, chunk])
+      // console.error('CHUNK ' + chunk)
+      remainder = null
+    }
+
+    // Write out this chunk
     const ret = current.write(chunk, enc)
 
-    if (fnBound(state, chunk)) {
-      // console.debug('end stream')
+    // If we reached the end, cycle the stream
+    if (off >= 0) {
       _flush(() => {
         index += 1
         // console.debug('next stream')
